39acbe8d1c3528771c1a2f1e0f284b515442916e
jesse <jessehj@slogup.com>
2021-08-03 21:20:12 +0900

    feat: Deploy with surge, remove gh-pages configurations

--- src/Routes.js
+++ src/Routes.js
@@ -4,13 +4,10 @@ import AtmMapDetailPage from "./pages/AtmMapDetailPage";
 
 export default function Routes() {
   return (
-    <Router basename={"https://jessehj.github.io/skn-mintit-map"}>
+    <Router>
       <Switch>
         <Route exact path="/" component={AtmMapPage} />
-        <Route
-          path="/detail"
-          component={AtmMapDetailPage}
-        />
+        <Route exact path="/detail" component={AtmMapDetailPage} />
       </Switch>
     </Router>
   );
